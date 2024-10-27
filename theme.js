3c63440882909d3627725786bce0b8c1f5a547a0
chyinan <60872708+chyinan@users.noreply.github.com>
2024-10-28 01:07:57 +0800

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,5 @@
+        /* 背景图片 */
+        html {
+            background: url('https://th.bing.com/th/id/R.0e8cf540458600c281d71039a16d0695?rik=XBAlTRuJVZRkiA&pid=ImgRaw&r=0') no-repeat center center fixed;
+            background-size: cover;
+        }
