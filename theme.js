543c45462256805589811c2740c1bda35e807eab
chyinan <60872708+chyinan@users.noreply.github.com>
2024-10-28 01:09:41 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,5 +1,5 @@
         /* 背景图片 */
         html {
-            background: url('https://th.bing.com/th/id/R.0e8cf540458600c281d71039a16d0695?rik=XBAlTRuJVZRkiA&pid=ImgRaw&r=0') no-repeat center center fixed;
+            background: url('https://img-baofun.zhhainiao.com/fs/0e8cf540458600c281d71039a16d0695.jpg') no-repeat center center fixed;
             background-size: cover;
         }
