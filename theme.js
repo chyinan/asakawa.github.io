4a62dec3f3ed951af0a34fab35aa298f37ae0e54
chyinan <60872708+chyinan@users.noreply.github.com>
2024-10-28 13:28:48 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
 html {
-    background: url('https://img-baofun.zhhainiao.com/fs/8b2124d8f4050f874743ad3e655e32ba.jpg') no-repeat center center fixed;
-    background-size: cover;
+    background: url('https://img-baofun.zhhainiao.com/fs/8b2124d8f4050f874743ad3e655e32ba.jpg') no-repeat center center fixed !important;
+    background-size: cover !important;
 }
