efd23367e3c0c71678aaa77c8302993e0e587488
chyinan <60872708+chyinan@users.noreply.github.com>
2024-11-16 14:30:17 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,4 +1,20 @@
-html {
-    background: url('https://img-baofun.zhhainiao.com/fs/8b2124d8f4050f874743ad3e655e32ba.jpg') no-repeat center center fixed !important;
-    background-size: cover !important;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    let style = document.createElement("style");
+    style.innerHTML = `
+        /* 背景图片 */
+        html {
+            background: url('https://i1.hdslb.com/bfs/archive/ab5a393bb7f5c8dee2c3a3db54dfff4c4d56540b.jpg') no-repeat center center fixed;
+            background-size: cover;
+        }
+        
+        /* 主体布局 - 为了让内容更易读 */
+        body {
+            margin: 30px auto;
+            padding: 20px;
+            background: rgba(255, 255, 255, 0.8); /* 白色背景，透明度80% */
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
+    `;
+    document.head.appendChild(style);
+}); 
